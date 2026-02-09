61a59be37813f3eae6d020dcc879042727df98f1
Lena0007 <122921197+Lena0007@users.noreply.github.com>
2026-02-09 22:01:22 +0100

    HW - 09

---
+++ src/js/2-form.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('.feedback-form');
+const key = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  formData[name] = value;
+  localStorage.setItem(key, JSON.stringify(formData));
+}
+
+const savedData = localStorage.getItem(key);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+  if (!formData.email || !formData.message) {
+    return;
+  }
+  console.log(formData);
+
+  localStorage.removeItem(key);
+  event.currentTarget.reset();
+
+  formData.email = '';
+  formData.message = '';
+}
